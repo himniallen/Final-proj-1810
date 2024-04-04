ee94e00fe776f3e5e76db02c80b93ab977ca94f7
Himni Allen <himni.allen@students.snow.edu>
2024-04-04 12:16:18 -0600

    class notes

---
+++ Dungen.js
@@ -0,0 +1,104 @@
+/* reset can be easy if your paranoid about your distinctions.
+poeple can change the schedule. then whatever they have done right now.
+we have some sort of a current schedule.
+to dimentional array in the domain.
+is update and rerender it
+so in your UI function you have  something like
+just asks domain for a copie of it.
+by workflow to some generated and then set to current schedule varible saying move this to that
+reset we will rerun the generation and rearagned from scratch to do it from current schedule.
+organize them to what you are attempting.
+and edit it to have an issue form the master coppy from being the default but is collection but the 
+the array of arrays that are empty
+starting in the UI we call reset and render in your reset button log things.
+going through and having things work to have it go and work.
+ that might help fix it is to have the things be seperated things to have 
+ this is interisting.
+ how can we adjust this to be better for mobile.
+ copies of the initial array without the contends
+ commented out things such as master and did it again to see what happens with the second filter f
+ foreach to remove and
+ unassigned and classes were both referencing the same array becuase they were almost always by reference
+    export the things to do it better for you. this is a function that will get the things to work and into a 
+    second varible and go into service you did a reassignment.
+    returns the api list and abouve it and no fetches is running on initilize.
+    copy the linse to have interisting things
+    list of classes.
+    length of 0 is interisting in how it works and how the log works but it can be expanded and length of 0
+
+    we just stoped having the get list
+    and just log them inividually.
+    goes to console.log e
+    function to assign new things
+    and how we have office hours things is interisting.
+    office hours becuase have to continue and dive into it
+    bonus points if we have the things to be done to have the proposals and uh go through how they work.
+    meet dead lines saturday you will die.
+    chess with chess api and when you have the things functionally and logic of what goes through to make sure that it is as hard until then
+    make sure that things work and ajusted them from before.
+    maybe have things mobile responsive to have extra credit before that there is an extra credit thing.
+    go from their.
+    filter based on what you have and possibly what you have.
+    building our own API. make sure that it is a good thing on each page.
+    so what we wants to implement is the clothes that we have and how to do it.
+    do a day were we do this intergrations.
+    integrating is much safe from legal perspective and pick a service that will do it later.
+    and then later do your own later.
+    such as things that is a concept and have different pages to host it that we will use blazor and have a c# API that can port over
+    and have it fall under different things that is running without user interaction.
+    series of websites that just run and don't work.
+    so be less of a disconnect and have it do what we want to do that is do this thing do with fetch and invoke things to do backend API stuff.
+    rugh timeline and have it labed and have things to be up to date.
+    reality not what was proposed the thing.
+    to get things with weather api todays and then weeks and maybe next two weeks.
+    try to use browser locatoin api that gets data.
+    on text and have the gps is worth hard things and one thing that is interisting
+    granularity of things and like to see one more thing and stor in local things.
+    but have them look at things that are stored and such.
+    recamendation of locations and things.
+    wouldn't have to do that.
+    hosting things and other service that you have made. multiple lobies and life cycle.
+    logic in c# is plenty of work is that it
+    that is the big difficulty there.
+    not inpossible but deffinatly hard.
+    we should know this before moble and could lot of data to manage but things to upload and have a class day
+    few things to teach but down to make things to do files external
+    go over how to go over things to do it in his schedule and how to fix it.
+
+    web site for authors that advertise to them and then have them show cased and have them work with portoflio
+    demonstration to do it.
+    one page to have drag and drop that were saying is to have the things form the website to have the books to store and add new books.
+    something like that.
+    like it portfolio sites ard cool.
+    basicly make a hub for the tournament and all sorts of things.
+    teams of three and how things work to have it five pages and information and information is a new thing 
+    page for team 
+    and how to have ADmin rules and how it works.
+    integrated with chess.coms api for the things to have in the form or have the intergration and go for it
+    haven't worked with before and you need to be start 
+    tool chain and double checking and try to have it wrok.
+    have an easier one to have themwork and day one is easier to work with and have something else in it.
+    just get into the vanilia version
+    what is he thinking 
+    dnd character manager is interistnig.
+    thing is that they have what supports it first is explosion of atributes to have these parts to focus on.
+
+    with fifteen minutes left would re recamend typescript to 
+    you just have to run the commands to go in when you get the things to ingnor ethings to domain 
+    that is interisting and it needs to just create how it will make the js files inline.
+    put the js files in folder and then inport them
+    compile ts to js
+    before the page reload
+    sound have used the this keyword.
+    ok private and public data and go object oriented more distinction and how things work
+    return stirngs things and return a list of course objects
+    how you use it is how to do it.
+    day on typescript.
+    see if you can fit it in and couple days on APIS next week.
+    go ahead and get started on tuesday.
+    
+
+
+
+
+ */
