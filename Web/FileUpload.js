b104ec6850db2c74eca27ae8a14f3915011ce641
Himni Allen <himni.allen@students.snow.edu>
2024-04-16 12:18:24 -0600

    class notes and org added

--- Web/FileUpload.js
+++ Web/FileUpload.js
@@ -21,3 +21,24 @@ const sendFileToAPIShouldBeInServiceFile = async (file) => {
     
   });
 };
+
+//if we get more than one that means we have a file and 
+/*
+maybe it will worck and have a foreacn 
+app.MapPost("/URL sub string", (http reciveiniten) => {what are you going to do aboutit})
+
+copy file to write stream instead of string fileName = "Uploaded.svg"
+save fileName = file.name
+using (var fileStrean = new FileStream(filename, fileMode.Create))
+{
+  file.CopyTo(fileStream);
+}
+
+demo is restared is interisthg.
+have thing form file uppload.
+this PNG file and is sent thier.
+page is reloading ani't it.
+page may or may not be reloading
+may be unwieldy out of hand c# vs js code to have it done know it is cool but not matters
+
+ */
