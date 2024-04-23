eebe8deef56624c99fe9905d1c17284c4da22d56
Himni Allen <himni.allen@students.snow.edu>
2024-04-23 12:17:05 -0600

    Class notes

--- Classnote.cs
+++ Classnote.cs
@@ -1,5 +1,33 @@
 /*
-
+javascprit was accidentally created
+put comment support
+the thing is done and insteristing
+being the first presenter
+the thing working
+can't access the thing to do it for us.
+tomarrow is the deploying to github and azure pages for depending for the next bit
+so the thing that you have been working on is
+just home page
+c# API to checkers
+can't pull it and something not togithub
+had git issues
+the hello string into logs on front end
+lets get it up and running
+then send the string to 
+some export functions the normal
+curly brakets
+and log its return UI.js lets console log yhe get messages
+and then calll with parenthesis
+it is tje anything error when it says cores
+c# crashed and something else
+we talked to the API it is something else
+the string itself is not js notation return an object for c# API don't return straight up/
+dynamic things are bad bur in this case it is the js syntaxe if the same it is a dynamic thing 
+restart it.
+should be brought in with javascripts
+network and you click on it you will have to 
+just had a weild filter and more dynamic things to do and get it to work.
+wrap in currly brackects response.content but in this case wrap it in an object 
 
 
 */
