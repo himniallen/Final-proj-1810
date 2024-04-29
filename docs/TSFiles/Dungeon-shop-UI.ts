cc6aeb73c6b91c8c67620f5de520c9ee79959b1b
himni allen <himni.allen@students.snow.edu>
2024-04-29 15:57:14 -0600

    final addidtions to help fix rubric

--- docs/TSFiles/Dungeon-shop-UI.ts
+++ docs/TSFiles/Dungeon-shop-UI.ts
@@ -1,6 +1,7 @@
 import {
     playerCharacter,
     GetWeaponFromAPI,
+    GetLocalStorageOfPlayer,
     
 } from "./Dungeon.Domain";
 
@@ -14,10 +15,13 @@ const armorForUser = document.getElementById("ArmorforUser") as HTMLElement;
 const armorForEquiping = document.getElementById("ArmorforEquiping") as HTMLElement;
 
 // Add event listeners for drag events to all draggable elements
-draggableElements.forEach((element: Element) => {
-    const draggableElement = element as HTMLElement; // Explicitly cast to HTMLElement
-    draggableElement.addEventListener("dragstart", dragStart);
-});
+function addEventListenerToGear()
+{
+    draggableElements.forEach((element: Element) => {
+        const draggableElement = element as HTMLElement; // Explicitly cast to HTMLElement
+        draggableElement.addEventListener("dragstart", dragStart);
+    });
+}
 
 // Add event listeners for drop events to both drop zones
 armorForUser.addEventListener("dragover", dragOver);
@@ -43,3 +47,35 @@ function drop(event: DragEvent): void {
     const dropZone = event.currentTarget as HTMLElement;
     dropZone.appendChild(draggedElement!);
 }
+
+
+function RenderGear(GearList: Array<JSON>)
+{
+    const target = document.getElementById("ArmorforEquiping") as HTMLElement;
+    target.innerHTML = "";
+
+    GearList.forEach((gear)  =>{
+        const Gear = document.createElement("div");
+        Gear.classList.add("Gear");
+        //@ts-ignore
+        Gear.innerText = gear.name;
+    });
+}
+
+
+function getQueryParam(paramName: string): string | null {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get(paramName);
+}
+
+const playerName = getQueryParam('name');
+if (playerName) {
+    GetLocalStorageOfPlayer(playerName);
+    console.log("name", playerName);
+} else {
+    console.log("name not found in query string.");
+}
+
+
+
